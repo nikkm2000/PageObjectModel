e72cab32d37715c43159e6bfc737264209367656
nikhil kumar <nikkm2000@gmail.com>
2023-01-21 12:40:26 +0530

    firstPOM

---
+++ src/main/java/com/crm/qa/pages/LoginPage.java
@@ -0,0 +1,60 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.crm.qa.base.TestBase;
+
+public class LoginPage extends TestBase{
+	
+	//Page Factory - OR:
+	@FindBy(name="username")
+	WebElement username;
+	
+	@FindBy(name="password")
+	WebElement password;
+	
+	@FindBy(xpath="//input[@type='submit']")
+	WebElement loginBtn;
+	
+	@FindBy(xpath="//button[contains(text(),'Sign Up')]")
+	WebElement signUpBtn;
+	
+	@FindBy(xpath="//img[contains(@class,'img-responsive')]")
+	WebElement crmLogo;
+	
+	//Initializing the Page Objects:
+	public LoginPage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Actions:
+	public String validateLoginPageTitle(){
+		return driver.getTitle();
+	}
+	
+//	WebDriverWait myWait= new WebDriverWait(driver, 10);	
+//	private By userName= By.name("username");
+	
+	public boolean validateCRMImage(){
+		//myWait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(userName));
+		return crmLogo.isDisplayed();
+	}
+	
+	public HomePage login(String un, String pwd){
+		username.sendKeys(un);
+		password.sendKeys(pwd);
+		//loginBtn.click();
+		    	JavascriptExecutor js = (JavascriptExecutor)driver;
+		    	js.executeScript("arguments[0].click();", loginBtn);
+		    	
+		return new HomePage();
+	}
+	
+}
